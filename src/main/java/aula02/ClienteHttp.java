4c5ef5b0e0c0df365df3335f0c00aa2d1804121d
gabrielle-1 <gabriellerodriguesmd@gmail.com>
2022-07-21 13:28:41 -0300

    Adicionando a classe de requisição

--- src/main/java/aula02/ClienteHttp.java
+++ src/main/java/aula02/ClienteHttp.java
@@ -1,2 +1,24 @@
-package aula02;public class ClienteHttp {
+package aula02;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ClienteHttp {
+
+    public String buscaDados (String url) {
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = null;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        String body = response.body();
+        return body;
+    }
 }
